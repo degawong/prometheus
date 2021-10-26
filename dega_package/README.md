1f4e2d7d710256821ef29c3b57a60f381ffc4efb
dega <degawong@126.com>
2021-10-26 11:13:01 +0800

    [fix](image_io): - fix image to video operation.

--- dega_package/README.md
+++ dega_package/README.md
@@ -1,10 +1,10 @@
 <!--
  * @Author: your name
  * @Date: 2021-09-07 20:09:11
- * @LastEditTime: 2021-09-07 20:10:48
+ * @LastEditTime: 2021-10-26 11:11:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dega_package\README.md
 -->
 
-`dega_imageio imagevideo not work yet.`
+`[ fixed ] dega_imageio imagevideo not work yet.`
--- dega_package/dega_imageio.py
+++ dega_package/dega_imageio.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-09-07 18:43:11
-LastEditTime: 2021-09-07 20:10:06
+LastEditTime: 2021-10-26 11:07:12
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \dega_package\dega_imageio.py
@@ -19,25 +19,34 @@ import dega_filesystem
 class dega_imageio():
     def __init__(self):
         self.__df = dega_filesystem.dega_filesystem()
-        self.__four_cc={
-            '.avi' : cv2.VideoWriter_fourcc('F','M','P','4'),
-            '.mp4' : cv2.VideoWriter_fourcc('M','J','P','G'),
+        self.__four_cc = {
+            # WMV1 WMV2 MJPG DIVX XVID MP4V ==> AVI
+            '.avi' : cv2.VideoWriter_fourcc(*'XVID'),
+            '.AVI' : cv2.VideoWriter_fourcc(*'XVID'),
+            '.mp4' : cv2.VideoWriter_fourcc(*'mp4v'),
+            '.MP4' : cv2.VideoWriter_fourcc(*'mp4v'),
         }
     def image_video(self, image_directory, video_path, fps=24):
         image_list = self.__df.walk_path(image_directory)
         assert(len(image_list) > 1)
         size = cv2.imread(image_list[0]).shape[0:2]
         (_, extension) = os.path.splitext(video_path)
-        video_writer = cv2.VideoWriter(video_path, self.__four_cc[extension], fps, size)
+        video_writer = cv2.VideoWriter(video_path, self.__four_cc[extension], fps, (size[1], size[0]))
         for image_path in image_list:
-            video_writer.write(cv2.imread(image_path))
+            image = cv2.imread(image_path)
+            video_writer.write(image)
         video_writer.release()
     def video_image(self, video_path, image_directory, image_extension='.bmp'):
         video = cv2.VideoCapture(video_path)
         for i in range(int(video.get(cv2.CAP_PROP_FRAME_COUNT))):
             _, frame = video.read()
-            cv2.imwrite(os.path.join(image_directory,'{:06d}'.format(i) + image_extension), frame) if _ is not None else None
+            cv2.imwrite(os.path.join(image_directory,'{:06d}'.format(i) + image_extension), frame) if frame is not None else None
 
 if __name__ == '__main__':
-    dega_imageio().video_image(image_directory='g:/share/image_io/image/', video_path='g:/share/image_io/000.mp4')
+    # dega_imageio().video_image(image_directory='f:/share/filter/000/', video_path='f:/share/filter/000.mov')
+    # dega_imageio().video_image(image_directory='f:/share/filter/001/', video_path='f:/share/filter/001.mov')
+    # dega_imageio().video_image(image_directory='g:/share/image_io/image/', video_path='g:/share/image_io/000.mp4')
     # dega_imageio().image_video(image_directory='g:/share/image_io/image/', video_path='g:/share/image_io/video/0.avi', fps=24)
+    dega_imageio().image_video(image_directory='f:/share/filter/', video_path='f:/share/filter/000.avi', fps=24)
+
+    pass
